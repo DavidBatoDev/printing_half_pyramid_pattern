fba4bf2ec0b5d18cdd9ff4b95307fe6c11d6de97
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-23 20:35:36 +0800

    Finished Code!

--- exercise14.py
+++ exercise14.py
@@ -1,6 +1,12 @@
 # Define a variable with the value 5
+i = 5
 # create a while loop with a condition of while the i is greater than 0
-# print the value of '*' multiplied by the value of i
+while i > 0:
+    # print the value of '*' value of i
+    print('*' * i)
+    # decrement i by 1
+    i -= 1
+
 
 
     
